4bb418fe1700e28e684818fbb7f9415b2741a851
Tango <tangzhenzhi@dreame.tech>
2025-01-24 18:01:37 +0800

    Initial Commit

---
+++ SwiftUIWithCombine/SwiftUIWithCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithCombineApp.swift
+//  SwiftUIWithCombine
+//
+//  Created by Tang Tango on 2025/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
